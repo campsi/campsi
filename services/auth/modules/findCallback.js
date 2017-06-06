ada250b211dac7d7acf502f5f6bf13c047b3aff1
Christophe Braud <christophe@agilitation.fr>
2017-06-06 19:54:49 +0200

    Extract all services from campsi delete unnecessary files

--- services/auth/modules/findCallback.js
+++
@@ -1,8 +0,0 @@
-module.exports = function findCallback(args) {
-    let i = 0;
-    for (; i < args.length; i++) {
-        if (typeof args[i] === 'function') {
-            return {callback: args[i], index: i};
-        }
-    }
-};
